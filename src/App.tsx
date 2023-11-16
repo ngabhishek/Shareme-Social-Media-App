e4e9be45268d9984d56696b68812707a161eaffc
Abhishek Kumar Gupta <cs.abhishek@gmail.com>
2023-11-16 23:07:56 +0530

    skeleton + basic routing

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { Routes, Route } from 'react-router-dom';
+
+import './globals.css';
+import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
+import { Home } from './_root/pages';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
+
+const App = () => {
+  return (
+    <main className='flex h-screen'>
+      <Routes>
+        {/*public routes*/}
+        <Route element={<AuthLayout />}>
+          <Route path='/sign-in' element={<SigninForm />}></Route>
+          <Route path='/sign-in' element={<SignupForm />}></Route>
+        </Route>
+        {/*private routes*/}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />}></Route>
+        </Route>
+      </Routes>
+    </main>
+  )
+}
+
+export default App
